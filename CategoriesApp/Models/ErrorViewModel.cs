cabcc236096daa8df1c8423b17e1ec47e307a06a
Romeo2005 <ua.roma2005@gmail.com>
2022-06-12 21:00:16 +0300

    Initial commit

---
+++ CategoriesApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CategoriesApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
